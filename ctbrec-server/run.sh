edff5fa0c19e1ee7f6166042de444de801937af6
p0rnb0x <moksic@protonmail.com>
2020-12-05 04:20:20 +0000

    3.10.6

--- ctbrec-server/run.sh
+++ ctbrec-server/run.sh
@@ -11,7 +11,7 @@ fi
 
 cd "${HOME}/server"
 java -Xmx2048m \
-     -cp "ctbrec-server-3.10.4-final.jar" \
+     -cp "ctbrec-server-3.10.6-final.jar" \
      -Dctbrec.config=server.json \
      ctbrec.recorder.server.HttpServer
 
